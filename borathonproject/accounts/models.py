f0deaebeb5cc8caa63ef7594a9ff674bdcd02132
son-nguyen-vmw <110196176+son-nguyen-vmw@users.noreply.github.com>
2022-07-28 13:36:31 -0700

    Activated accounts model

--- borathonproject/accounts/models.py
+++ borathonproject/accounts/models.py
@@ -4,7 +4,7 @@ from shortuuidfield import ShortUUIDField
 
 # Create your models here.
 class Account(models.Model):
-    id = ShortUUIDField(unique=True)
+    account_id = ShortUUIDField(unique=True)
     account_number = ShortUUIDField(unique=True)
     balance = models.FloatField(default=0.00)
     account_status = models.BinaryField()
