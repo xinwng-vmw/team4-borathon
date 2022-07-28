3e4ea5995e01577371f83d11bc0ab0dba80069b3
Song Zheng <songz1@vmware.com>
2022-07-28 13:25:01 -0700

    customer model

--- borathonproject/customers/models.py
+++ borathonproject/customers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from shortuuidfield import ShortUUIDField
 
 # Create your models here.
+class Customer(models.Model):
+    id = ShortUUIDField(unique=True)
+    first_name= models.CharField()
+    last_name= models.CharField()
+    associated_account=models.ForeignKey("Account")
