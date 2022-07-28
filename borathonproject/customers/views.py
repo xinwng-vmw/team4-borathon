6e90d01532a81bc684c32b60da052c961fe5f042
Song Zheng <songz1@vmware.com>
2022-07-28 14:21:27 -0700

    Customer view

--- borathonproject/customers/views.py
+++ borathonproject/customers/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Customer
+def index(request):
+    return HttpResponse("This is the customer view")
 
 # Create your views here.
