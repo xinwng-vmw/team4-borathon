a62a8a3b0f77dbbf95047ad274b22fe7855dce5b
Xin Wang <xin@xwang3-a01.vmware.com>
2022-07-28 13:16:32 -0700

    transaction model

--- borathonproject/transactions/models.py
+++ borathonproject/transactions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from shortuuidfield import ShortUUIDField
 # Create your models here.
+
+
+class Transaction(models.Model):
+    id = ShortUUIDField(unique=True)
+    amount = models.FloatField(default=0.00)
+    type = models.BinaryField()  # 0 for Credit, 1 for Debit
+    # not too sure about the foreign key
+    associated_account = models.ForeignKey("Account")
