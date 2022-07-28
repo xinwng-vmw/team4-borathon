8e974f72ac7a941c150b9843c8ef89f3a69e60b3
son-nguyen-vmw <110196176+son-nguyen-vmw@users.noreply.github.com>
2022-07-28 14:19:46 -0700

    Added view for accounts

--- borathonproject/accounts/views.py
+++ borathonproject/accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Account
 
 # Create your views here.
+def index(request):
+    return HttpResponse("You're currently at the accounts view!")
